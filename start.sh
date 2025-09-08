83cb857b39ad41a6f6da32f0ac1b8f91221b7e3c
masplay120 <fachacarlos@hotmail.com>
2025-09-08 20:43:55 -0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+# Variables de entorno (se pueden setear en Render)
+INPUT_URL=${INPUT_URL:-"https://live20.bozztv.com/giatvplayout7/giatv-208566/playlist.m3u8"}
+OUTPUT_URL=${OUTPUT_URL:-"rtmp://estacionmixtv:159357456@36bay2.tulix.tv/giatv-estacionmixtv/estacionmixtv"}
+VIDEO_SIZE=${VIDEO_SIZE:-1280x720}
+VIDEO_BITRATE=${VIDEO_BITRATE:-3000k}
+AUDIO_BITRATE=${AUDIO_BITRATE:-64k}
+
+echo "▶️ Iniciando restream..."
+echo "   Origen: $INPUT_URL"
+echo "   Destino: $OUTPUT_URL"
+echo "   Resolución: $VIDEO_SIZE"
+echo "   Bitrate: $VIDEO_BITRATE / Audio: $AUDIO_BITRATE"
+
+ffmpeg -re -i "$INPUT_URL" \
+-c:v libx264 -preset veryfast -b:v $VIDEO_BITRATE -maxrate $VIDEO_BITRATE -bufsize 2M -s $VIDEO_SIZE -r 30 -pix_fmt yuv420p \
+-c:a aac -b:a $AUDIO_BITRATE -ar 48000 -ac 2 \
+-f flv "$OUTPUT_URL"
