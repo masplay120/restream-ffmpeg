1e6594d2018843759d1f10c5e3b11f24a608c10b
masplay120 <fachacarlos@hotmail.com>
2025-09-08 21:52:37 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -15,6 +15,8 @@ echo "   Video bitrate: $VIDEO_BITRATE"
 echo "   Audio bitrate: $AUDIO_BITRATE"
 
 exec ffmpeg -re -i "$INPUT_URL" \
-  -c:v libx264 -preset veryfast -b:v $VIDEO_BITRATE -maxrate $VIDEO_BITRATE -bufsize 2M -s $VIDEO_SIZE -r 30 -pix_fmt yuv420p \
-  -c:a aac -b:a $AUDIO_BITRATE -ar 48000 -ac 2 \
-  -f flv "$OUTPUT_URL"
+-c:v libx264 -preset veryfast -b:v $VIDEO_BITRATE -maxrate $VIDEO_BITRATE -bufsize 2M -s $VIDEO_SIZE -r 30 -pix_fmt yuv420p \
+-c:a aac -b:a $AUDIO_BITRATE -ar 48000 -ac 2 \
+-f flv "$OUTPUT_URL" \
+-hide_banner -loglevel warning \
+-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 30
