743c6f7aa3177d244e0513c2329d4d0c2ca4ef1f
masplay120 <fachacarlos@hotmail.com>
2025-09-24 02:07:33 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ set -e
 INPUT_URL=${INPUT_URL:-"https://live20.bozztv.com/giatvplayout7/giatv-208566/tracks-v1a1/mono.ts.m3u8"}
 OUTPUT_URL=${OUTPUT_URL:-"rtmp://ssh101.bozztv.com:1935/ssh101/estacionmixtv"}
 VIDEO_SIZE=${VIDEO_SIZE:-1280x720}
-VIDEO_BITRATE=${VIDEO_BITRATE:-800k}
+VIDEO_BITRATE=${VIDEO_BITRATE:-500k}
 AUDIO_BITRATE=${AUDIO_BITRATE:-64k}
 
 while true; do
@@ -14,7 +14,7 @@ while true; do
     -fflags +genpts \
     -timeout 10000000 \
     -i "$INPUT_URL" \
-    -c:v libx264 -preset veryfast -b:v "$VIDEO_BITRATE" -maxrate 800k -bufsize 3000k -s "$VIDEO_SIZE" -r 30 -pix_fmt yuv420p \
+    -c:v libx264 -preset veryfast -b:v "$VIDEO_BITRATE" -maxrate 500k -bufsize 3000k -s "$VIDEO_SIZE" -r 30 -pix_fmt yuv420p \
     -c:a aac -b:a "$AUDIO_BITRATE" -ar 44100 -ac 2 \
     -f flv "$OUTPUT_URL" \
     -hide_banner -loglevel error
