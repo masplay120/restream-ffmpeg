dc256676abf8c8c70074a587040afaaaf39a6908
masplay120 <fachacarlos@hotmail.com>
2025-09-24 01:53:51 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,13 +7,18 @@ VIDEO_SIZE=${VIDEO_SIZE:-1280x720}
 VIDEO_BITRATE=${VIDEO_BITRATE:-1000k}
 AUDIO_BITRATE=${AUDIO_BITRATE:-64k}
 
-echo "▶️ Restream iniciando..."
-echo "   Origen: $INPUT_URL"
-echo "   Destino: $OUTPUT_URL"
+while true; do
+  echo "▶️ [$(date)] Iniciando restream desde $INPUT_URL a $OUTPUT_URL..."
 
-exec ffmpeg \
--i "$INPUT_URL" \
--c:v libx264 -preset veryfast -b:v "$VIDEO_BITRATE" -maxrate 1500k -bufsize 3000k -s "$VIDEO_SIZE" -r 30 -pix_fmt yuv420p \
--c:a aac -b:a "$AUDIO_BITRATE" -ar 44100 -ac 2 \
--f flv "$OUTPUT_URL" \
--hide_banner -loglevel warning
+  ffmpeg \
+    -fflags +genpts \
+    -timeout 10000000 \
+    -i "$INPUT_URL" \
+    -c:v libx264 -preset veryfast -b:v "$VIDEO_BITRATE" -maxrate 1500k -bufsize 3000k -s "$VIDEO_SIZE" -r 30 -pix_fmt yuv420p \
+    -c:a aac -b:a "$AUDIO_BITRATE" -ar 44100 -ac 2 \
+    -f flv "$OUTPUT_URL" \
+    -hide_banner -loglevel error
+
+  echo "⚠️ [$(date)] FFmpeg terminó. Reintentando en 5 segundos..."
+  sleep 5
+done
