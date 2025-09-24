3b81821aa700113efdcb080df7086025da687cc3
masplay120 <fachacarlos@hotmail.com>
2025-09-24 01:41:09 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,14 +10,10 @@ AUDIO_BITRATE=${AUDIO_BITRATE:-64k}
 echo "▶️ Restream iniciando..."
 echo "   Origen: $INPUT_URL"
 echo "   Destino: $OUTPUT_URL"
-echo "   Resolución: $VIDEO_SIZE"
-echo "   Video bitrate: $VIDEO_BITRATE"
-echo "   Audio bitrate: $AUDIO_BITRATE"
 
-exec ffmpeg -re -i "$INPUT_URL" \
--c:v libx264 -preset veryfast -b:v 1000k -maxrate 2000k -bufsize 4M -s 1280x720 -r 30 -pix_fmt yuv420p \
--c:a aac -b:a 64k -ar 48000 -ac 2 \
--f flv -flvflags no_duration_filesize "$OUTPUT_URL" \
--hide_banner -loglevel warning \
--reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 30
-
+exec ffmpeg \
+-i "$INPUT_URL" \
+-c:v libx264 -preset veryfast -b:v "$VIDEO_BITRATE" -maxrate 1500k -bufsize 3000k -s "$VIDEO_SIZE" -r 30 -pix_fmt yuv420p \
+-c:a aac -b:a "$AUDIO_BITRATE" -ar 44100 -ac 2 \
+-f flv "$OUTPUT_URL" \
+-hide_banner -loglevel warning
