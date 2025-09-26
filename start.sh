ef4f5091f80ab2c3a75e1b34c76fd2a0d2c3b6b7
masplay120 <fachacarlos@hotmail.com>
2025-09-25 23:15:42 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
 
-# Variables (se pueden sobrescribir en Render -> Environment Variables)
 INPUT_URL=${INPUT_URL:-"https://live20.bozztv.com/giatvplayout7/giatv-208566/playlist.m3u8"}
 OUTPUT_URL=${OUTPUT_URL:-"rtmp://streamlive8.hearthis.at/live/10778826?secret=b45494c4g584"}
 
 while true; do
-  echo "▶️ [$(date)] Iniciando restream (COPIA DIRECTA) desde $INPUT_URL a $OUTPUT_URL..."
+  echo "▶️ [$(date)] Restream en vivo (copiando fluido) desde $INPUT_URL a $OUTPUT_URL..."
 
   ffmpeg \
+    -fflags +genpts+discardcorrupt \
+    -use_wallclock_as_timestamps 1 \
+    -protocol_whitelist "file,http,https,tcp,tls" \
     -reconnect 1 \
     -reconnect_streamed 1 \
     -reconnect_delay_max 30 \
+    -rtbufsize 256M \
     -i "$INPUT_URL" \
     -c copy \
     -f flv "$OUTPUT_URL" \
